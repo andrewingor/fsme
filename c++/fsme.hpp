405d5fbd3afb5a4cfbb945101bb67f4757034cbf
Andre Wingor <andrewingor@gmail.com>
2018-02-22 05:32:39 -0500

    Call foreign non-class function (no good)

--- c++/fsme.hpp
+++ c++/fsme.hpp
@@ -2,48 +2,71 @@
 //
 
 #include <exception>
+#include <vector>
+#include <string>
 #include <map>
 
 using namespace std;
 
 namespace fsm {
 
-//---------------
-/* fsme ----------------
-	Virtual base state of fsme
+//---------
+/* engine ----------------
+	Base of FSM
 --------------------------------*/
-class fsme {
+template<  typename __iEvent
+	 , typename __oEvent
+	 , typename ___State >
+ class engine:
+	  public basic_istream<__iEvent>
+	, public basic_ostream<__oEvent> {
 private:
+	//
 public:
-	 fsme() { }
-virtual ~fsme() { }
+	 engine() { }
+virtual ~engine() { }
 
-virtual void	Go(void*) const = 0;
+const string&	rev() const {
+		static string Rev = ("$Id$"); return Rev; }
+
+	typedef __iEvent	event_input_type;
+virtual void	go(__iEvent) = 0;
 };
 
-//---------------
+//----------
 /* bot ----------------
-	FSME Robot
+	FSM Engine robot
 --------------------------------*/
-template<typename Event>
+template< typename __iEvent = uint8_t
+	, typename __oEvent = __iEvent
+	, typename ___State = __iEvent >
  class bot:
-	  public fsme
-	, public basic_istream<Event>
-	, public basic_ostream<Event> {
+	  public engine<__iEvent, __oEvent, ___State>{
 private:
-	typedef Event		event_type;
+	//
+public:
+	typedef void(*Handle)(___State*);
+	typedef	map<__iEvent, Handle>	___Handles;
+	typedef	vector<___Handles>	___Table;
+	typedef ___State		state_type;
+	typedef __iEvent		event_input_type;
+	typedef __oEvent		event_output_type;
 
-Event	state;
+___State	Current;
+___Table	States;
 
 public:
-map<Event, void(*)(Event)> handler;
-
-	 bot () { }
+	bot () { }
 virtual	~bot () { }
+virtual	void	go(__iEvent Event) { States[Current][Event](&Current); }
 
-//	void	Handler(Event e, void(func*)() ) const { handler[e] = func; }
-	void	Go(void*) const { }
+	void transit(___State trans) { Current = trans; }
+
+typename ___Table::iterator& state_begin() const { return States.begin(); }
+typename ___Table::iterator& state_end()  const { return States.end(); }
+___Table& State() { return States; }
+
 };
-
 
 }//namespace fsm
+
